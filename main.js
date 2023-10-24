568e782221ce1bce13fc54eab8b759f545d700fc
luis gomez <luisgomezojeda110502@gmail.com>
2023-10-24 09:24:25 -0300

    correction route

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import { serviceWorker } from "./serviceWorker"
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
+  navigator.serviceWorker.register('./public/sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
